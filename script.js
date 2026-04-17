042ff65c5f5f8bc2a319c650d8f451c87e57a4b0
siddhant2621 <siddhantmore86670@gmail.com>
2026-04-17 19:10:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
